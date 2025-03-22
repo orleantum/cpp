dcf5d32767f44be61f4badf7ebd857d6c154d984
Gucci Mane Love JavaScript <114758683+orleantum@users.noreply.github.com>
2025-03-22 18:36:46 +0500

    Add files via upload

---
+++ 7.2/7.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int sum(int a, ...) {
+    int sum = 0;
+    int* p = &a;
+    while (*p) {
+        sum += *p;
+        p += 2;
+    }
+
+    return sum;
+}
+
+int main() {
+    cout << sum(1, 2, 3, 0) << endl;
+    cout << sum(1, 2, 3, 4, 5, 6, 7, 0) << endl;
+    cout << sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0) << endl;
+    return 0;
+}
