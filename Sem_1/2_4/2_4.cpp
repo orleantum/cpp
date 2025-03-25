953583cdb96ee750c71061414609c7e1bcf74635
Gucci Mane Love JavaScript <114758683+orleantum@users.noreply.github.com>
2025-03-25 10:23:11 +0500

    Add files via upload

---
+++ Sem_1/2_4/2_4.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, mid, h;
+    cin >> N;
+    mid = N / 2; // Центр
+    h = N - mid - 2; // Высота
+
+    if (N > 3)
+    {
+        // Вершина
+        for (int i = 0; i < mid; i++)
+        {
+            cout << " ";
+        }
+
+        for (int i = mid; i < N - mid; i++)
+        {
+            cout << "*";
+        }
+
+        for (int i = mid + 1; i < N; i++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+        int counter = 1;
+
+        // Стороны
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < mid - counter; j++)
+            {
+                cout << " ";
+            }
+            
+            cout << "*";
+
+            for (int j = mid - counter; j < mid + counter - 1; j++)
+            {
+                cout << " ";
+            }
+
+            cout << "*";
+
+            counter++;
+            cout << endl;
+        }
+
+        // Основание
+        for (int i = 0; i < N; i++)
+        {
+            cout << "*";
+        }
+    }
+    else
+    {
+        cout << "N must be more than 3!";
+    }
+}
