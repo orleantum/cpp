0448138b10427ee2f00488ff52396f5aa29e896b
Gucci Mane Love JavaScript <114758683+orleantum@users.noreply.github.com>
2025-03-25 10:23:59 +0500

    Add files via upload

---
+++ Sem_1/2_2/2_2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, mult = 1;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        int a = rand() % 10 + 1;
+        mult *= a;
+        cout << a << " ";
+    }
+
+    cout << endl << mult;
+}
