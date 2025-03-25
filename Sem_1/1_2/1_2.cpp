fb8865e3cde4eea539a685baa98f312f163fae67
Gucci Mane Love JavaScript <114758683+orleantum@users.noreply.github.com>
2025-03-25 10:25:21 +0500

    Add files via upload

---
+++ Sem_1/1_2/1_2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b;
+
+    cin >> a >> b;
+    
+    if (b == 0)
+    {
+        cout << "Cannot divide by zero!" << endl;
+    }
+    else
+    {
+        cout << a / b << endl;
+    }
+}
