afbcd23f861b5693cdd483faddb9935251a9dd37
Gucci Mane Love JavaScript <114758683+orleantum@users.noreply.github.com>
2025-03-25 10:23:33 +0500

    Add files via upload

---
+++ Sem_1/2_3/2_3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, counter = 3, mult, sum = 0;
+
+    cin >> N;
+
+    cout << "S = ";
+
+    for (int i = 0; i < N; i++)
+    {
+        mult = 1;
+        for (int j = i + 1; j < counter; j++)
+        {
+            mult *= j;
+            if (j < counter - 1)
+            {
+                cout << j << " * ";
+            }
+            else
+            {
+                cout << j;
+            }
+        }
+        sum += mult;
+        if (i < N - 1)
+        {
+            cout << " + ";
+        }
+        counter += 2;
+    }
+
+    cout << " = " << sum;
+}
