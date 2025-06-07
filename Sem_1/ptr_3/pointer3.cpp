ac0e6730d49a625e1d257182918083757fbf9d07
Gucci Mane Love JavaScript <114758683+orleantum@users.noreply.github.com>
2025-06-07 20:32:19 +0500

    Add files via upload

---
+++ Sem_1/ptr_3/pointer3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float n,c;
+    float *ptr1;
+    float *ptr2;
+    cin >> n >> c;
+    ptr1 = &n;
+    ptr2 = &c;
+    cout << *ptr1 + *ptr2 << endl;
+    return 0;
+}
+
